4bbdbc711724d4c13306bf3669bb33c5490f5ce1
Geektree0101 <h2s1880@gmail.com>
2021-01-16 21:21:33 +0900

    init

---
+++ src/main/java/com/example/geektree/youmingdifang/YoumingdifangApplication.java
@@ -0,0 +1,13 @@
+package com.example.geektree.youmingdifang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoumingdifangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoumingdifangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geektree/youmingdifang/YoumingdifangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geektree.youmingdifang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoumingdifangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
